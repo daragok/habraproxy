e51531b1ce185c72a8ad1ee169ff60cc46f2c9ef
Sergei <graser777@gmail.com>
2019-06-20 23:02:55 +0300

    add test for update_html_font_paths function

--- mainapp/tests.py
+++ mainapp/tests.py
@@ -3,7 +3,7 @@ import unittest
 
 import bs4
 
-from mainapp.views import collect_font_urls_from_html
+from mainapp.views import collect_font_urls_from_html, update_html_font_paths
 
 
 class TestHabraProxyMethods(unittest.TestCase):
@@ -16,6 +16,20 @@ class TestHabraProxyMethods(unittest.TestCase):
                                      "/fonts/0/FiraSans/firaSans-medium.woff",
                                      "/fonts/0/FiraSans/firaSans-medium.woff2",
                                      "/fonts/0/FiraSans/firaSans-medium.eot"}
+                         )
+
+    def test_update_html_font_paths(self):
+        font_urls = {"/fonts/0/FiraSans/firaSans-medium.ttf",
+                     "/fonts/0/FiraSans/firaSans-medium.woff",
+                     "/fonts/0/FiraSans/firaSans-medium.woff2",
+                     "/fonts/0/FiraSans/firaSans-medium.eot"}
+        soup = self._get_mock_html_soup('test_update_html_font_paths.html')
+        update_html_font_paths(soup, font_urls)
+        updated_font_urls = collect_font_urls_from_html(soup)
+        self.assertEqual(updated_font_urls, {'/static/mainapp/fonts/0/FiraSans/firaSans-medium.eot',
+                                             '/static/mainapp/fonts/0/FiraSans/firaSans-medium.ttf',
+                                             '/static/mainapp/fonts/0/FiraSans/firaSans-medium.woff',
+                                             '/static/mainapp/fonts/0/FiraSans/firaSans-medium.woff2'}
                          )
 
     def _get_mock_html_soup(self, name):
