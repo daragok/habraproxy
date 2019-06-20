60ddd927b74f3cfa59936396d704062f9101943d
Sergei <graser777@gmail.com>
2019-06-21 00:55:23 +0300

    add test_update_html_svg_paths

--- mainapp/tests.py
+++ mainapp/tests.py
@@ -3,7 +3,8 @@ import unittest
 
 import bs4
 
-from mainapp.views import collect_font_urls_from_html, update_html_font_paths, collect_svg_urls_from_html
+from mainapp.views import collect_font_urls_from_html, update_html_font_paths, collect_svg_urls_from_html, \
+    update_html_svg_paths
 
 
 class TestHabraProxyMethods(unittest.TestCase):
@@ -36,6 +37,12 @@ class TestHabraProxyMethods(unittest.TestCase):
         soup = self._get_mock_html_soup('test_collect_svg_urls_from_html.html')
         svg_urls = collect_svg_urls_from_html(soup)
         self.assertEqual(svg_urls, {'https://habr.com/images/1560786911/common-svg-sprite.svg'})
+
+    def test_update_html_svg_paths(self):
+        soup = self._get_mock_html_soup('test_update_html_svg_paths.html')
+        update_html_svg_paths(soup)
+        updated_svg_urls = collect_svg_urls_from_html(soup)
+        self.assertEqual(updated_svg_urls, {'/static/mainapp/images/1560786911/common-svg-sprite.svg'})
 
     def _get_mock_html_soup(self, name):
         html_path = os.path.join(self.mocks, name)
