b0d648ae49da5775cf7bc7863bbf8a08c058bd93
Sergei <graser777@gmail.com>
2019-06-21 01:17:33 +0300

    add test_add_trademarks_to_all_words_of_length_six

--- mainapp/tests.py
+++ mainapp/tests.py
@@ -1,10 +1,11 @@
 import os
+import re
 import unittest
 
 import bs4
 
 from mainapp.views import collect_font_urls_from_html, update_html_font_paths, collect_svg_urls_from_html, \
-    update_html_svg_paths, update_habr_urls_to_my_server_urls
+    update_html_svg_paths, update_habr_urls_to_my_server_urls, add_trademarks_to_all_words_of_length_six
 
 
 class TestHabraProxyMethods(unittest.TestCase):
@@ -54,6 +55,18 @@ class TestHabraProxyMethods(unittest.TestCase):
                                  'http://127.0.0.1:8003/ru/news/',
                                  'http://127.0.0.1:8003/ru/sandbox/',
                                  'http://127.0.0.1:8003/ru/users/'})
+
+    def test_add_trademarks_to_all_words_of_length_six(self):
+        def _get_clean_html_text(soup):
+            t = soup.text.replace('\n', ' ')
+            t = re.sub(' +', ' ', t)
+            return t.strip(' ')
+
+        soup = self._get_mock_html_soup('test_add_trademarks_to_all_words_of_length_six.html')
+        add_trademarks_to_all_words_of_length_six(soup)
+        t = _get_clean_html_text(soup)
+        self.assertEqual(
+            t, 'Новости Песочница В понедельник Falcon™ Heavy запустит на орбиту™ спутник с солнечным парусом Яндекс™')
 
     def _get_mock_html_soup(self, name):
         html_path = os.path.join(self.mocks, name)
